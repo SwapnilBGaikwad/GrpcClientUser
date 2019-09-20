99980f8a746d628505a641a7333cb612a72ffb66
Swapnil Gaikwad <swapnil.gaikwad@go-jek.com>
2019-09-20 14:24:25 +0530

    Use proto client instead of generating client

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # GrpcClientUser
+
+Added 3 dependency to work.
+<br>
+1. GRPC API for making GRPC call
+2. Add protobuf model dependency
+3. Client with stub for client and serer
+<br>
+
+
+    compile files('libs/GrpcStub-1.0-SNAPSHOT.jar')
+    implementation "io.grpc:grpc-protobuf:${grpcVersion}"
+    implementation "io.grpc:grpc-stub:${grpcVersion}"
--- src/main/java/com/github/grpcuser/CalculatorClient.java
+++ src/main/java/com/github/grpcuser/CalculatorClient.java
@@ -9,12 +9,12 @@ import io.grpc.ManagedChannelBuilder;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
-public class CalculatorClient {
+class CalculatorClient {
     private static final Logger logger = Logger.getLogger(CalculatorClient.class.getName());
     private final ManagedChannel channel;
     private final CalculatorGrpc.CalculatorBlockingStub calculatorBlockingStub;
 
-    public CalculatorClient(String host, int port) {
+    CalculatorClient(String host, int port) {
         ManagedChannel channel = ManagedChannelBuilder.forAddress(host, port)
                 .usePlaintext()
                 .build();
@@ -23,13 +23,13 @@ public class CalculatorClient {
     }
 
 
-    public void execute() {
+    void execute() {
         Request request = Request.newBuilder().setA(1).setB(4).build();
         Response response = calculatorBlockingStub.sum(request);
         logger.info("Sum of 1 and 4 is " + response.getResult());
     }
 
-    public void shutdown() throws InterruptedException {
+    void shutdown() throws InterruptedException {
         channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }
 }
